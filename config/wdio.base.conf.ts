525e9183ff162fa59d394e3facdb06cf784c8102
Priyanka Shah <priyanka.shah@techholding.co>
2025-09-18 15:58:00 +0530

    minor update

--- config/wdio.base.conf.ts
+++ config/wdio.base.conf.ts
@@ -51,8 +51,8 @@ export const config: Options.Testrunner = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: ["../test/specs/**/*.ts"],
-  // specs: ["../test/specs/login/forgotPassword.spec.ts"],
+  // specs: ["../test/specs/**/*.ts"],
+  specs: ["../test/specs/login/forgotPassword.spec.ts"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
